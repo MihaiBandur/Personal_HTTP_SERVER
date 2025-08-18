4d8e04a6f2c4b1446374566cc280a53fd40cba39
MihaiBandur <mihaibandur224@gmail.com>
2025-08-18 16:38:25 +0300

    Working on main

--- http_server/main.c
+++ http_server/main.c
@@ -22,3 +22,45 @@ static const struct option long_options[] = {
 /* Description of short options for getopt_long. */
 
 static const char* const short_options = "a:hm:p:v";
+
+/* Usage summary text. */
+
+
+static const char* const usage_template =
+    "Usage: %s [ options ]\n"
+    " -a, --address ADDR Bind to local address (by default, bind\n"
+    " to all local addresses).\n"
+    " -h, --help Print this information.\n"
+    " -m, --module-dir DIR Load modules from specified directory\n"
+    " (by default, use executable directory).\n"
+    " -p, --port PORT Bind to specified port.\n"
+    " -v, --verbose Print verbose messages.\n";
+
+/* Print usage information and exit. If IS_ERROR is nonzero, write to
+stderr and use an error exit code. Otherwise, write to stdout and
+use a non-error termination code. Does not return. */
+
+static void print_message(int is_error){
+    fprintf (is_error ? stderr : stdout, usage_template, program_name);
+    exit (is_error ? 1 : 0);
+}
+
+int main(int argc, char** argv){
+    struct in_addr local_address;
+    uint16_t port;
+    int next_option;
+    
+    /* Store the program name, whick we'll use in a error message. */
+    program_name = argv[0];
+    /* Set defaults for options. Bind the server to all local addresses,
+    and assign an unused port automatically. */
+    local_address.s_addr = INADDR_ANY;
+    port = 0;
+    /* Don’t print verbose messages. */
+    verbose = 0;
+    /* Load modules from the directory containing this executable. */
+    module_dir = get_self_executable_directory ();
+    assert (module_dir != NULL);
+    
+    
+}
