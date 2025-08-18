e60c81878e5276d787dae1754b220c136f0c4838
MihaiBandur <mihaibandur224@gmail.com>
2025-08-18 17:08:41 +0300

    finished main

--- http_server/main.c
+++ http_server/main.c
@@ -40,7 +40,7 @@ static const char* const usage_template =
 stderr and use an error exit code. Otherwise, write to stdout and
 use a non-error termination code. Does not return. */
 
-static void print_message(int is_error){
+static void print_usage(int is_error){
     fprintf (is_error ? stderr : stdout, usage_template, program_name);
     exit (is_error ? 1 : 0);
 }
@@ -53,7 +53,7 @@ int main(int argc, char** argv){
     /* Store the program name, whick we'll use in a error message. */
     program_name = argv[0];
     /* Set defaults for options. Bind the server to all local addresses,
-    and assign an unused port automatically. */
+     and assign an unused port automatically. */
     local_address.s_addr = INADDR_ANY;
     port = 0;
     /* Don’t print verbose messages. */
@@ -62,5 +62,91 @@ int main(int argc, char** argv){
     module_dir = get_self_executable_directory ();
     assert (module_dir != NULL);
     
+    /* Parse options*/
+    do{
+        next_option =
+        getopt_long(argc, argv, short_options, long_options, NULL);
+        switch(next_option){
+            case 'a':
+                /* User specified -a or --address. */
+            {
+                struct hostent* local_host_name;
+                
+                /* Lock up the hostname the user specified. */
+                local_host_name = gethostbyname(optarg);
+                if(local_host_name == NULL || local_host_name->h_length == 0){
+                    /* Could not resolve the name. */
+                    error(optarg, "Invalid host name");
+                }
+                else
+                /* Hostname is OK, so use it. */
+                    local_address.s_addr = *((int*) (local_host_name->h_addr_list[0]));
+            }
+                break;
+                
+            case 'h':
+                /* User specified -h or --help. */
+                print_usage(0);
+            case 'm':
+                /* User specified -m or --module-dir. */
+            {
+                struct stat dir_info;
+                /* Check that it exists. */
+                if(access(optarg, F_OK) != 0)
+                    error(optarg, "module directory does not exist");
+                /* Check that is accesible. */
+                if(access(optarg, R_OK | X_OK) != 0)
+                    error (optarg, "module directory is not accessible");
+                /* Make sure that it is a dir ectory. */
+                if(stat(optarg, &dir_info) != 0 || !S_ISDIR(dir_info.st_mode))
+                    error(optarg, "not a directory");
+                /* It looks OK, so use it. */
+                module_dir = strdup(optarg);
+            }
+                break;
+            case 'p':
+                /* User specified -p or --port. */
+            {
+                long value;
+                char* end;
+                
+                value = strtol(optarg, &send, 10);
+                if(*end != '\0')
+                /* The user specified nondigits in the port number. */
+                    print_usage(1);
+                /* The port number needs to be converted to network (big endian)
+                 byte order. */
+                port = (uint16_t)htons(value);
+            }
+                break;
+            case 'v':
+                /* User specified -v or --verbose. */
+                verbose = 1;
+                break;
+            case '?':
+                /* User specified an unrecognized option. */
+                print_usage(1);
+            case  -1:
+                /* Done with options. */
+                break;
+            default:
+                abort();
+                
+        }
+    }while(next_option != -1);
     
+    /* This program takes no additional arguments. Issue an error if the
+     user specified any. */
+    
+    if(optind != argc)
+        print_usage(1);
+    
+    /* Print the module directory, if we’re running verbose. */
+    if(verbose)
+        printf ("modules will be loaded from %s\n", module_dir);
+    
+    /* Run the server. */
+    
+    server_run(local_address, port);
+    return 0;
 }
